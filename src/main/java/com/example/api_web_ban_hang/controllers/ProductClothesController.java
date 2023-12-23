0d70e5f7590003fe528bcccf58a6305cb459cf1e
Trần Tuyên <20130459@st.hcmuaf.edu.vn>
2023-12-23 15:02:30 +0700

    thêm các api mới trong ProductClothesController

--- src/main/java/com/example/api_web_ban_hang/controllers/ProductClothesController.java
+++ src/main/java/com/example/api_web_ban_hang/controllers/ProductClothesController.java
@@ -7,12 +7,16 @@ import com.example.api_web_ban_hang.dto.ProductDTO_Ver1;
 import com.example.api_web_ban_hang.filter.PaginationFilter;
 import com.example.api_web_ban_hang.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
 
 /**
  * Controller này dùng để handle các API liên quan đến sản phẩm quần áo
@@ -28,20 +32,71 @@ public class ProductClothesController {
     }
 
     @GetMapping("/ds-ao-da-banh-moi")
-    public PagedResponse_Ver1<List<ProductDTO_Ver1>> getListSoccerShirtNew(@RequestParam(defaultValue = "1") int page,
-                                                                           @RequestParam(defaultValue = "15") int pageSize) {
+    public ResponseEntity<PagedResponse_Ver1<List<ProductDTO_Ver1>>> getListSoccerShirtNew(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "15") int pageSize) {
         try {
             var validFilter = new PaginationFilter(page, pageSize);
 
             var data = _productService.getListProductByTypeAndStatus(TypeProduct.AO_DAU, StatusProduct.MOI, validFilter.current_page, validFilter.page_size);
 
+            if (data == null || data.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+
             var total_items = _productService.countProductsByTypeAndStatus(TypeProduct.AO_DAU, StatusProduct.MOI);
 
-            return new PagedResponse_Ver1<List<ProductDTO_Ver1>>(data, validFilter.current_page, validFilter.page_size, total_items);
+            return ResponseEntity.ok(new PagedResponse_Ver1<>(data, validFilter.current_page, validFilter.page_size, total_items));
 
         } catch (Exception e) {
-            return null;
+            // Xử lý lỗi và trả về mã trạng thái 500
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
 
+    @GetMapping("/ds-ao-da-banh-hot")
+    public ResponseEntity<PagedResponse_Ver1<List<ProductDTO_Ver1>>> getListSoccerShirtHot(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "15") int pageSize) {
+        try {
+            var validFilter = new PaginationFilter(page, pageSize);
+
+            var data = _productService.getListProductByTypeAndStatus(TypeProduct.AO_DAU, StatusProduct.MOI, validFilter.current_page, validFilter.page_size);
+
+            if (data == null || data.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+
+            var total_items = _productService.countProductsByTypeAndStatus(TypeProduct.AO_DAU, StatusProduct.MOI);
+
+            return ResponseEntity.ok(new PagedResponse_Ver1<>(data, validFilter.current_page, validFilter.page_size, total_items));
+
+        } catch (Exception e) {
+            // Xử lý lỗi và trả về mã trạng thái 500
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @GetMapping("/ds-ao-da-banh-khuyen-mai")
+    public ResponseEntity<PagedResponse_Ver1<List<ProductDTO_Ver1>>> getListSoccerShirtPromotional(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "15") int pageSize) {
+        try {
+            var validFilter = new PaginationFilter(page, pageSize);
+
+            var data = _productService.getListProductByTypeAndStatus(TypeProduct.AO_DAU, StatusProduct.MOI, validFilter.current_page, validFilter.page_size);
+
+            if (data == null || data.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+
+            var total_items = _productService.countProductsByTypeAndStatus(TypeProduct.AO_DAU, StatusProduct.MOI);
+
+            return ResponseEntity.ok(new PagedResponse_Ver1<>(data, validFilter.current_page, validFilter.page_size, total_items));
+
+        } catch (Exception e) {
+            // Xử lý lỗi và trả về mã trạng thái 500
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
 }
