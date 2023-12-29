4f2e6b4877720b3ecee3d82a077ce3f0377a9ee2
ntandat20130221 <20130221@st.hcmuaf.edu.vn>
2023-12-29 15:29:17 +0700

    Add create product API

--- src/main/java/com/example/api_web_ban_hang/models/entities/Product.java
+++ src/main/java/com/example/api_web_ban_hang/models/entities/Product.java
@@ -2,6 +2,7 @@ package com.example.api_web_ban_hang.models.entities;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
@@ -13,6 +14,7 @@ import java.util.Set;
 
 @Getter
 @Setter
+@ToString
 @Entity
 @Table(name = "products")
 public class Product {
