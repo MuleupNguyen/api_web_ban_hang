f31c9446586737d21fe4888b2cb2e445ecf6aa55
Lenh Nguyen <20130303@st.hcmuaf.edu.vn>
2024-01-02 10:33:31 +0700

    fix search product and comment v2

--- src/main/java/com/example/api_web_ban_hang/controllers/ProductApi.java
+++ src/main/java/com/example/api_web_ban_hang/controllers/ProductApi.java
@@ -1,13 +1,24 @@
 package com.example.api_web_ban_hang.controllers;
 
-import com.example.api_web_ban_hang.models.ResponseObject;
-import com.example.api_web_ban_hang.services.interfaces.IProductService;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
+import org.springframework.data.web.SortDefault.SortDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import com.example.api_web_ban_hang.models.ResponseObject;
+import com.example.api_web_ban_hang.services.interfaces.IProductService;
 
 @RestController
 @RequestMapping("/api/products")
@@ -15,6 +26,7 @@ import java.util.Optional;
 public class ProductApi {
 	@Autowired
 	private IProductService productService;
+
 
 	@GetMapping("/infor-product/{id}")
 	public ResponseEntity<ResponseObject> findProductById(@PathVariable(name = "id") long id) {
@@ -35,5 +47,19 @@ public class ProductApi {
 								.limit(quantity != null ? quantity : Long.MAX_VALUE))
 						.status(HttpStatus.OK.name()).message(HttpStatus.OK.getReasonPhrase()).build());
 	}
+
+	@GetMapping("/fitter-product-hot")
+	public ResponseEntity<ResponseObject> findProductHotByBrand(@RequestParam(name = "brand") String brand,
+			@PageableDefault(size = 3, page = 0) @SortDefaults({
+					@SortDefault(sort = "listedPrice", direction = Sort.Direction.DESC) }) Pageable pageable) {
+		return Optional.ofNullable(
+				ResponseEntity.ok()
+				.body(new ResponseObject("Ok", "OK", 
+						productService.findProductByBrandWithOptionSort(brand,pageable))))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+						new ResponseObject(HttpStatus.NOT_FOUND.name(), HttpStatus.NOT_FOUND.getReasonPhrase(), "")))
+				;
+	}
+	
 
 }
