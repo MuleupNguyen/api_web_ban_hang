7f9d297ffe98ece60918c2f8f3bc1688d71b6c8c
Nguyen-Du-Lap <20130302@st.hcmuaf.edu.vn>
2024-01-02 12:51:16 +0700

    chuc nang tao don hang moi

--- src/main/java/com/example/api_web_ban_hang/controllers/OrderController.java
+++ src/main/java/com/example/api_web_ban_hang/controllers/OrderController.java
@@ -8,59 +8,42 @@ import com.example.api_web_ban_hang.models.entities.Product;
 import com.example.api_web_ban_hang.repos.OrderDetailRepository;
 import com.example.api_web_ban_hang.repos.OrderRepository;
 import com.example.api_web_ban_hang.repos.ProductRepository;
+import com.example.api_web_ban_hang.services.interfaces.OrderDetailService;
+import com.example.api_web_ban_hang.services.interfaces.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/order")
+@CrossOrigin("*")
 public class OrderController {
     @Autowired
-    OrderRepository orderRepo;
-
+    private OrderService orderService;
     @Autowired
-    OrderDetailRepository orderDetailRepo;
-
-    @Autowired
-    ProductRepository productRepo;
-
+    private OrderDetailService orderDetailService;
     @Transactional
-    @RequestMapping("/create-order")
-    public ResponseObject create(@RequestBody @Valid OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setIdStatusOrder(1);
-        order.setToName(orderRequest.getName_customer());
-        order.setToPhone(orderRequest.getPhone());
-        order.setEmailCustomer(orderRequest.getEmail_customer());
-        order.setToAddress(orderRequest.getTo_address().getAddress());
-        order.setToWardName(orderRequest.getTo_address().getWard_name());
-        order.setToDistrictName(orderRequest.getTo_address().getDistrict_name());
-        order.setToProvinceName(orderRequest.getTo_address().getProvince_name());
-        order.setToWardId(orderRequest.getTo_address().getWard_id());
-        order.setToDistrictId(orderRequest.getTo_address().getDistrict_id());
-        order.setToProvinceId(orderRequest.getTo_address().getProvince_id());
-        order.setNote(orderRequest.getNote());
-        order.setShipPrice(orderRequest.getShip_price());
-        order.setOrderValue(orderRequest.getOrder_value());
-        order.setTimeOrder(LocalDateTime.now());
-        orderRepo.save(order);
+    @PostMapping("/create-order")
+    public ResponseEntity<ResponseObject> create(@RequestBody @Valid OrderRequest orderRequest) {
+        Order order = orderService.addOrder(orderRequest);
+
 
         orderRequest.getList_order_detail().stream().forEach(o -> {
-            OrderDetail orderDetail = new OrderDetail();
-            orderDetail.setProduct(productRepo.findProductById(o.getId_product()));
-            orderDetail.setNameSize(o.getNameSize());
-            orderDetail.setQuantity(o.getQuantity());
-            orderDetail.setPrice(o.getPrice());
-            orderDetail.setOrder(order);
-            orderDetailRepo.save(orderDetail);
+            orderDetailService.addOrderDetail(o, order);
         });
-        return new ResponseObject("OK", "Tạo đơn hàng thành công!", orderRequest);
+        return Optional.of(ResponseEntity.ok().body(
+                new ResponseObject(
+                        HttpStatus.OK.name(),
+                        HttpStatus.OK.getReasonPhrase(),
+                        orderRequest
+                )
+        )).get();
     }
 
 }
--- src/main/java/com/example/api_web_ban_hang/models/OrderRequest.java
+++ src/main/java/com/example/api_web_ban_hang/models/OrderRequest.java
@@ -1,12 +1,17 @@
 package com.example.api_web_ban_hang.models;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
+@Builder
 public class OrderRequest {
     private String name_customer;
     private String phone;
@@ -15,5 +20,6 @@ public class OrderRequest {
     private String note;
     private BigDecimal ship_price;
     private BigDecimal order_value;
+    private BigDecimal total_price;
     private Set<OrderDetailRequest> list_order_detail = new HashSet<>();
 }
